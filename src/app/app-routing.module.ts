f1867f50f084ee4eb63ac154f947cb2a988494e8
Michael Burger <michael.burger@orma-solutions.it>
2019-03-19 16:10:13 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,40 @@
+import { Comp3Component } from './comp3/comp3.component';
+import { Comp2Component } from './comp2/comp2.component';
+import { Comp1Component } from './comp1/comp1.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    children: [
+      {
+        path: '',
+        loadChildren: './home/home.module#HomeModule'
+      }
+    ]
+  },
+  {
+    path: 'comp1',
+    outlet: 'secondary',
+    component: Comp1Component
+  },
+  {
+    path: 'comp2',
+    outlet: 'secondary',
+    component: Comp2Component
+  },
+  {
+    path: 'comp3',
+    outlet: 'secondary',
+    component: Comp3Component
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,6 @@
+import { Comp3Component } from './comp3/comp3.component';
+import { Comp2Component } from './comp2/comp2.component';
+import { Comp1Component } from './comp1/comp1.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -6,7 +9,7 @@ import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, Comp1Component, Comp2Component, Comp3Component
   ],
   imports: [
     BrowserModule,
